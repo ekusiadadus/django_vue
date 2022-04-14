61c3c9c19ab54f81795468c232a7caaa637b5409
ekusiadadus <ekusiadadus@gmail.com>
2022-04-14 13:35:55 +0000

    add: vue views

--- web/frontend/django_rest/src/main.ts
+++ web/frontend/django_rest/src/main.ts
@@ -1,6 +1,9 @@
+import axios from "axios";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+axios.defaults.baseURL = "http://127.0.0.1:5003";
 
 createApp(App).use(store).use(router).mount("#app");
--- web/frontend/django_rest/src/views/HomeView.vue
+++ web/frontend/django_rest/src/views/HomeView.vue
@@ -1,12 +1,67 @@
 <template>
-  <div class="home">Home</div>
+  <div class="home">
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">Welcome to ekusiadadus</p>
+        <p class="subtitle">The best game store online</p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        v-bind:key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail" />
+          </figure>
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HomeView",
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
   components: {},
+  mounted() {
+    this.getLatestProducts();
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get("api/v1/latest-products/")
+        .then((res) => {
+          this.latestProducts = res.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 });
 </script>
+
+<style scoped>
+.image {
+  margin-top: -1.25rem;
+  margin-left: -1.25rem;
+  margin-right: -1.25rem;
+}
+</style>
